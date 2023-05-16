29d1ad400cc8d47e9e4bfb28838a3c4d4b4917d6
Richard <achonyerichard@gmail.com>
2023-05-16 11:54:08 +0100

    hannah

--- src/components/blog/modal/ModalThreeBlogContent.jsx
+++ src/components/blog/modal/ModalThreeBlogContent.jsx
@@ -8,63 +8,65 @@ const ModalThreeBlogContent = () => {
         <h1>
           Post <span>Details</span>
         </h1>
-        <span className="title-bg">posts</span>
+        <span className="title-bg">Hannah 🤩</span>
       </div>
       {/* Meta Starts */}
 
       <div className="meta open-sans-font">
         <span>
-          <i className="fa fa-user"></i> steve
+          <i className="fa fa-user"></i> for my lover
         </span>
         <span className="date">
-          <i className="fa fa-calendar"></i> 9 January 2017
+          <i className="fa fa-calendar"></i> May 16, 2023
         </span>
         <span>
-          <i className="fa fa-tags"></i> wordpress, business, economy, design
+          <i className="fa fa-tags"></i> medieval, loml, yamhead, bathe
         </span>
       </div>
       {/* Meta Ends */}
       {/* Article Content Starts */}
 
-      <h1>Everything You Need to Know About Web Accessibility</h1>
-      <img src="img/blog/blog-post-3.jpg" className="img-fluid" alt="Blog" />
+      <h1>The Tale of my Queen</h1>
+      <img src="img/blog/blog-post-3.jpeg" className="img-fluid" alt="Blog" />
       <div className="blog-excerpt open-sans-font pb-5">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          If love blows across time, my love for you shall be the wind that
+          forever guideth me to your heart.I pen this epistle with my heart full
+          and my mind aching for blooming efferverscence of my Queen.In world so
+          cold and ever so dark your beauty and grace flourishes amidst the
+          tapestries of time, this woman of extraordinary allure makes me long
+          for her presence ever so more.
         </p>
         <div className="quotebox">
           <div className="icon">
             <img src="img/blog/quote.svg" alt="blog quote" />
           </div>
           <p>
-            Most photographers find it hard to see interesting pictures in
-            places in which they are most familiar. A trip somewhere new seems
-            always exactly what our photography needed, as shooting away from
-            home consistently inspires us to new artistic heights.
+            Not only heralded for her physical beauty that captivated hearts; it
+            was her inner humor that made even the greatest jester sit down and
+            laugh. Her heart, pure and compassionate and as big as her voluptous
+            breasts(wik wink)enchantend me ,oh my I'm a mess.
           </p>
         </div>
         <p>
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-          officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna.
+          My ever so beautiful Queen you make this nigh forever have a standing
+          sword(wink wink again). And i shall thrust your enemies down just to
+          lay you down and thrust even more
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua.
+          The semen demon, my blow job queen(oh yeah its getting sexual) drain
+          me only shall i dream. When nights get cold and springs get warm we
+          shall forever sin
+        </p>
+        <p>
+          My Queen, the embodyment of  courage, compassion,
+          and intellect, stands as my beacon of inspiration and admiration in this
+          medieval English love confession. She was a muse for poets, a symbol of strength
+          for warriors, and a gentle guiding light for those fortunate enough to
+          cross her path. In the tapestry of medieval England, this woman, both
+          captivating and hardworking, stood as a living testament to the
+          timeless beauty that transcends the ages, leaving an indelible mark
+          upon the hearts and minds of all who beheld her.{" "}And i shall end this not as medieval English but with Pidgin."Omo you de burst my head every day"
         </p>
       </div>
       {/* Article Content Ends */}
